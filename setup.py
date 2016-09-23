36ab1cbf32f3b89334e61f42bfd850ef164ddbc0
Dave Voutila <voutilad@gmail.com>
2016-09-23 12:14:06 -0400

    updated package files

--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ setup(
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     package_data={
-        'reporters_db': [
+        'judge_pics': [
             'data/*',
+            'judges.json',
         ]
     },
     classifiers=[
