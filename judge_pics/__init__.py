155cd1fdb5e2f7cd24f18bf55dfabd0f615b9612
Dave Voutila <voutilad@gmail.com>
2016-09-23 12:07:12 -0400

    refactor of project layout.

--- judge_pics/__init__.py
+++ judge_pics/__init__.py
@@ -2,6 +2,5 @@ import json
 import os
 
 judge_root = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(judge_root, 'judges.json')) as f:
+with open(os.path.join(judge_root, 'data', 'judges.json')) as f:
     judge_pics = json.load(f)
-
