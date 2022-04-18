2491d57d75f735c4ff5081eba650562d3d813380
William Palin <bill@free.law>
2022-04-18 16:52:21 -0400

    feat(search): Add search by judge name

--- judge_pics/search.py
+++ judge_pics/search.py
@@ -1,12 +1,14 @@
 import json
 import os
+import re
 from enum import Enum
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Literal, Optional
+from typing import Literal, Optional, List, Union
 
 import climage
 import requests
+from fuzzywuzzy import fuzz
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -26,12 +28,42 @@ SIZES = Literal[
 ]
 
 
-def portrait(person: int, size: SIZES = ImageSizes.ORIGINAL) -> Optional[str]:
+def query(search_str: str, size: SIZES = ImageSizes.MEDIUM) -> Optional[List]:
+    """Find a judge by name"""
+    paths = [j["path"] for j in judges]
+
+    xlist = []
+    for path in paths:
+        matching_path = re.sub("[\d-]+", " ", path).strip()
+        m = fuzz.token_sort_ratio(matching_path, search_str.lower())
+        xlist.append((path, m))
+        if m > 95:
+            return [f"https://portraits.free.law/v2/{size}/{path}.jpeg"]
+    xlist.sort(key=lambda y: -y[1])
+    if len(xlist) == 0:
+        return None
+
+    return [
+        f"https://portraits.free.law/v2/{size}/{x[0]}.jpeg"
+        for x in xlist
+        if x[1] > 10
+    ]
+
+
+def portrait(
+    person: Union[str, int], size: SIZES = ImageSizes.ORIGINAL
+) -> Optional[str]:
     """Get URL for portait on free.law"""
-    paths = [x for x in judges if x["person"] == person]
-    if len(paths) > 0:
-        return f"https://portraits.free.law/v2/{size.value}/{paths[0]['path']}.jpeg"
+    if type(person) == int:
+        paths = [x for x in judges if x["person"] == person]
+        if len(paths) > 0:
+            return f"https://portraits.free.law/v2/{size.value}/{paths[0]['path']}.jpeg"
+        else:
+            return None
     else:
+        matches = query(person, size.value)
+        if matches:
+            return matches[0]
         return None
 
 
