91bd109b97d0a2e0790399f0fb3e72f9dd8e7024
Adam Johnson <me@adamj.eu>
2025-05-27 19:12:08 +0100

    test: Improve tests

--- tests.py
+++ tests.py
@@ -1,16 +1,62 @@
-import os
-import random
 from unittest import TestCase
 
-import climage
+from judge_pics.search import ImageSizes, portrait, query, show
 
 
-class RandomJudgePortrait(TestCase):
-    def test_random_judge_portrait(self):
-        """Can we show a random judge portrait"""
-        file = random.choice(os.listdir("judge_pics/data/orig/"))
-        fp = f"judge_pics/data/orig/{file}"
-        print(climage.convert(fp, width=40))
+class QueryTests(TestCase):
+    def test_found_confident(self):
+        result = query("Vadas Nandor", ImageSizes.SMALL.value)
+        self.assertEqual(
+            result,
+            ["https://portraits.free.law/v2/128/vadas-nandor.jpeg"],
+        )
+
+    def test_found_not_confident(self):
+        result = query("A", ImageSizes.LARGE.value)
+        self.assertIsInstance(result, list)
+        self.assertGreater(len(result), 0)
+
+    def test_not_found(self):
+        result = query("", ImageSizes.SMALL.value)
+        self.assertEqual(result, [])
+
+
+class PortraitTests(TestCase):
+    def test_int_found(self):
+        result = portrait(10156, ImageSizes.SMALL)
+        self.assertEqual(
+            result,
+            "https://portraits.free.law/v2/128/vadas-nandor.jpeg",
+        )
+
+    def test_int_found_large(self):
+        result = portrait(10156, ImageSizes.LARGE)
+        self.assertEqual(
+            result,
+            "https://portraits.free.law/v2/512/vadas-nandor.jpeg",
+        )
+
+    def test_int_not_found(self):
+        result = portrait(-1, ImageSizes.SMALL)
+        self.assertIsNone(result)
+
+    def test_str_found(self):
+        result = portrait("Vadas Nandor", ImageSizes.SMALL)
+        self.assertEqual(
+            result,
+            "https://portraits.free.law/v2/128/vadas-nandor.jpeg",
+        )
+
+    def test_str_not_found(self):
+        result = portrait("", ImageSizes.SMALL)
+        self.assertIsNone(result)
+
+
+class ShowTests(TestCase):
+    def test_show(self):
+        result = show(10156, ImageSizes.SMALL)
+        self.assertIsInstance(result, str)
+        self.assertTrue(result.startswith("\x1b[48;5;234m"))
 
 
 if __name__ == "__main__":
